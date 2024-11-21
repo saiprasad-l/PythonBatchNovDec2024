30c2c9f600869d469f3175fc6dc4b3a48db3c195
Sai Prasad L <prasadl@udel.edu>
2024-11-21 01:36:43 +0000

    Class02 Part 2

--- 01_Introduction/c_dynamictyping.py
+++ 01_Introduction/c_dynamictyping.py
@@ -11,6 +11,52 @@ print("num1 = ", num1)
 
 print("num1 =", num1, "type =", type(num1))
 
+#works in both static and dynamic typing
 num1 = 300
 print("num1 =", num1,"type =", type(num1))
+
+#python is a dynamic type language
+num1 = 300.0
+print("num1 =", num1, "type =", type(num1))
+
+num1 = 300.
+print("num1 =", num1, "type =", type(num1))
+
+num1 = 300,
+print("num1 =", num1, "type =", type(num1))
+
+num1 = (300,)
+print("num1 =", num1, "type =", type(num1))
+
+print()
+
+num1 = 300.0
+print("num1 =", num1, "type =", type(num1))
+
+num1 = -0.09
+print("num1 =", num1, "type =", type(num1))
+
+num1 = -0.09j
+print("num1 =", num1, "type =", type(num1))
+print()
+
+num1 = "300"
+print("num1 =", num1, "type =", type(num1))
+
+num1 = "three"
+print("num1 =", num1, "type =", type(num1))
+print()
+
+#num1 = true Python is Case Sensitive
+num1 = True
+print("num1 =", num1, "type =", type(num1))
+
+num1 = False
+print("num1 =", num1, "type =", type(num1))
+
+num1 = "NONE"
+print("num1 =", num1, "type =", type(num1))
+
+num1 = "none"
+print("num1 =", num1, "type =", type(num1))
 
