a66bdc2053a3f2069f6ba28f51a6841b7087a398
Sai Prasad L <prasadl@udel.edu>
2024-11-21 19:18:56 +0000

    Class02 Last Update

--- 01_Introduction/f_print_syntax.py
+++ 01_Introduction/f_print_syntax.py
@@ -16,6 +16,11 @@ print("Name of Student is ", name, sep=" ")
 print("Name of Student is ", name, sep="-")
 print("Name of Student is ", name, sep="")
 
+
+print(1, 2, 3, 4, 5, 6)
+print(1, 2, 3, 4, 5, 6, sep=" ")
+
+
 print(1, 2, 3, 4, 5, 6, sep="~")
 print(1, 2, 3, 4, 5, 6, sep="_")
 print(1, 2, 3, 4, 5, 6, sep=":")
@@ -31,3 +36,19 @@ print("1 + 2                =", 1+2)
 print("'1'  +  '2'          =", "1" + "2")
 print()
 
+# NOTE: Python is a strictly typed language
+print("1 + 2 =", 1 + 2) # addition
+print("'1' + '2' =", "1" + "2") # string concatenation
+print()
+
+# 1 + '2' # TypeError: unsupported operand type(s) for +: 'int' and 'str'
+# type converters str(), int(), float()
+print("1 + int('2') =", 1 + int("2"))
+print("str(1) + '2' =", str(1) + "2")
+print()
+
+
+print("int ('234') =", int("234"))
+
+# print("int('23.4T =", int('23.4')) # ValueError: invalid literal for il
+# print("int('two') int('two')) # ValueError: invalid literal for int
