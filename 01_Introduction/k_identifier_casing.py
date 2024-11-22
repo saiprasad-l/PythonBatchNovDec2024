bef6fd0152bc682da147545ee792e61358946dab
Sai Prasad L <prasadl@udel.edu>
2024-11-22 06:34:27 +0000

    Class03 Update 2

---
+++ 01_Introduction/k_identifier_casing.py
@@ -0,0 +1,39 @@
+
+
+
+
+# Python is case-sensitive language
+animal = "DOG"
+print(animal)
+
+# print(Animal)
+
+
+ANIMAL = "PIG"
+print (ANIMAL)
+
+Animal = "Camel"
+print(Animal)
+
+
+# variable casing
+# 1.srake casing or underscore casing
+
+student = "shiva"
+employee_salary = 2343223432.23
+cost_of_mango = 12
+selling_price_of_apples = 34
+
+output_of_thermal_sensor = 32
+no_of_current_processes = 5
+
+
+
+# 2. Camel casing
+Student = "Shiva"
+EmployeeSalary = 2343223432.23
+CostOfMango = 12
+SellingPriceofApples = 34
+
+OutputofThermalSensor = 32
+NoofCurrentProcesses = 5
